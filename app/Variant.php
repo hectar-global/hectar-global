6e562e9273647c1f1ac5ac2d5cf054f7d52f340c
manoj437 <manoj437@gmail.com>
2022-03-26 08:35:55 +0530

    Price section created

--- app/Variant.php
+++ app/Variant.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Price;
 use Illuminate\Database\Eloquent\Model;
 
 class Variant extends Model
@@ -11,5 +12,11 @@ class Variant extends Model
     public function product()
     {
         return $this->belongsTo('App\Product', 'product_id', 'id');
+    }
+
+    public function price()
+    {
+        return $this->hasMany(Price::class);
+
     }
 }
